3effe511fd987aef1ef854d7af3bedade53f6ccc
Artur Gudiev <arturgudiev>
2025-11-20 11:04:54 -0800

    add bash template

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,13 +1,16 @@
 import { Template } from "src/types/tempalte.interface.js";
 import BOOTSTRAP_TEMPLATES from "./bootstrap.tempaltes.js";
 import FLUTTER_TEMPLATES from "./flutter.templates.js";
+import BASH_TEMPLATES from "./bash.templates.js";
 
 
 export { default as BOOTSTRAP_TEMPLATES } from "./bootstrap.tempaltes.js";
 export { default as FLUTTER_TEMPLATES } from "./flutter.templates.js";
+export { default as BASH_TEMPLATES } from "./bash.templates.js";
 
 
 export const TEMPLATES_SET: {[key: string]: Template[]} = {
     'Bootstrap': BOOTSTRAP_TEMPLATES,
     'Flutter': FLUTTER_TEMPLATES,
+    'Bash': BASH_TEMPLATES,
 };
