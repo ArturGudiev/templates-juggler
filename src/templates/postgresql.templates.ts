08b3417b93be0c7daedcf0878eff58c8ae4ee760
Artur Gudiev <arturgudiev93@gmail.com>
2026-03-30 10:56:06 +0300

    Commit everything

--- src/templates/postgresql.templates.ts
+++ src/templates/postgresql.templates.ts
@@ -4,30 +4,9 @@ export default [
     {
         title: 'Remove database connections',
         content: `
-    <mat-select [(value)]="status">
-        @for (option of options; track option) {
-          <mat-option [value]="option">{{ option }}</mat-option>
-        }
-      </mat-select>
+    SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'chpo'
+  AND pid <> pg_backend_pid();
 
-
-    status = model('Active'); 
-    options = ['Active', 'Pending', 'Archived'];
-`
-    },
-    {
-        title: 'Select as form field',
-        content: `
-    <mat-form-field style="width: 10rem">
-      <mat-label>Logs group type</mat-label>
-      <mat-select [(value)]="groupType">
-        <mat-option [value]="'container'">Container</mat-option>
-        <mat-option [value]="'global'">Global</mat-option>
-        <mat-option [value]="'all'">All</mat-option>
-      </mat-select>
-    </mat-form-field>
-
-      groupType = model<'container' | 'global' | 'all'>('container')
 `
     }
 ] as Template[];
