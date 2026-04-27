cf1df9893ac61d786f0fecb2cc451d5ae6f98c39
Artur Gudiev <arturgudiev>
2026-04-26 22:29:00 -0700

    Hello here

--- src/templates/flutter.templates.ts
+++ src/templates/flutter.templates.ts
@@ -1,34 +1,46 @@
 import { Template } from "../types/template.interface.js";
 
 export default [
-    { title: 'Filter requests', content: 't:json'},
-    {
-        title: 'Flutter Button1',
-        content: `Button(child: Text('123'))`
-    },
-     {
-        title: 'CLI install packages',
-        content: `flutter pub get`
-    },
-    {
-        title: 'CLI install specific version',
-        content: `flutter pub add package_name:1.2.3`
-    },
-    {
-        title: 'Flutter Button 2',
-        content: `
+  { title: "Filter requests", content: "t:json" },
+  {
+    title: "Flutter Button1",
+    content: `Button(child: Text('123'))`,
+  },
+  {
+    title: "CLI install packages",
+    content: `flutter pub get`,
+  },
+  {
+    title: "CLI install specific version",
+    content: `flutter pub add package_name:1.2.3`,
+  },
+  {
+    title: "Flutter Button 2",
+    content: `
             Button(
                 child: Text('2222')    
             )  
-        `
-    },
-        {
-        title: 'Elevated Button',
-        content: `
+        `,
+  },
+  {
+    title: "Elevated Button",
+    content: `
         ElevatedButton(
             onPressed: _toggleIncidentsPanel,
             child: Text('Инциденты'),
         ),
-`
-    }
+`,
+  },
+  {
+    title: "SVG icon (from assets)",
+    content: `
+        SvgPicture.asset(
+          iconPath,
+          width: iconWidth,
+          height: iconHeight,
+          colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+        )
+`,
+  }
+
 ] as Template[];
