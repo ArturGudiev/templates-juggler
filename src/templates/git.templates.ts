8771e0705368cbf66d4d75d102824eb3359b73d4
Artur Gudiev <arturgudiev>
2026-05-25 00:25:12 -0700

    Git tempalte

--- src/templates/git.templates.ts
+++ src/templates/git.templates.ts
@@ -9,4 +9,16 @@ export default [
         title: 'Print default editor',
         content: 'git var GIT_EDITOR',
     },
+    {
+        title: 'Git set editor',
+        content: `
+        git config --global core.editor "code --wait"
+
+        # for merge
+        git config --global merge.tool vscode
+git config --global mergetool.vscode.cmd "code --wait --merge \$LOCAL \$REMOTE \$BASE \$MERGED"
+
+
+        `,
+    },
 ] as Template[];
